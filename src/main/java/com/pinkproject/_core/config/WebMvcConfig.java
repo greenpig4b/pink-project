860f06fe18dd315fafb931c22069defa41d4178a
kimjihun <greenpig4b@naver.com>
2024-07-08 12:33:15 +0900

    기초ã세팅

--- src/main/java/com/pinkproject/_core/config/WebMvcConfig.java
+++ src/main/java/com/pinkproject/_core/config/WebMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-
+//
 @Configuration // IoC
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
