51b0b397cc3dd6fea4b55a665493d236134d0786
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-07-08 15:11:34 +0900

    인서트 중

--- src/main/java/com/pinkproject/transaction/TransactionRequest/SaveTransactionDTO/_SaveTransactionDTO.java
+++ src/main/java/com/pinkproject/transaction/TransactionRequest/SaveTransactionDTO/_SaveTransactionDTO.java
@@ -1,4 +1,19 @@
 package com.pinkproject.transaction.TransactionRequest.SaveTransactionDTO;
 
-public record _SaveTransactionDTO() {
+import com.pinkproject.transaction.enums.Assets;
+import com.pinkproject.transaction.enums.CategoryIn;
+import com.pinkproject.transaction.enums.CategoryOut;
+import com.pinkproject.transaction.enums.TransactionType;
+
+public record _SaveTransactionDTO(
+        Integer userId,
+        TransactionType transactionType,
+        String yearMonthDate,
+        String time,
+        Integer amount,
+        CategoryIn categoryIn,
+        CategoryOut categoryOut,
+        Assets assets,
+        String description
+) {
 }
