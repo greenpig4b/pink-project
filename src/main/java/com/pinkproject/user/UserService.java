15bb8c5afb94c767389549cffc18bf75030bc016
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-07-10 12:45:45 +0900

    유저패키지에 각종 빈 추가

---
+++ src/main/java/com/pinkproject/user/UserService.java
@@ -0,0 +1,10 @@
+package com.pinkproject.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+}
