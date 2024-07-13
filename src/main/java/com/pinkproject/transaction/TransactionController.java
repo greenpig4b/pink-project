1df96229c8bef589f5877232b7356cbc1a4a3a64
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-07-13 16:33:30 +0900

    세션 빼고 api 빼고

--- src/main/java/com/pinkproject/transaction/TransactionController.java
+++ src/main/java/com/pinkproject/transaction/TransactionController.java
@@ -6,7 +6,6 @@ import com.pinkproject.transaction.TransactionRequest._UpdateTransactionRecord;
 import com.pinkproject.transaction.TransactionResponse._MonthlyTransactionMainRecord;
 import com.pinkproject.transaction.TransactionResponse._SaveTransactionRespRecord;
 import com.pinkproject.transaction.TransactionResponse._UpdateTransactionRespRecord;
-import com.pinkproject.user.SessionUser;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,47 +20,47 @@ public class TransactionController {
     // 월간 transaction 랜더링
     @GetMapping("/api/transactions/monthly")
     public ResponseEntity<?> monthlyTransactions(@RequestParam Integer year, @RequestParam Integer month) {
-         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
-         if (sessionUser == null) {
-             return ResponseEntity.status(401).build();
-         }
-        _MonthlyTransactionMainRecord respDTO = transactionService.getMonthlyTransactionMain(sessionUser.getId(), year, month);
+//         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+//         if (sessionUser == null) {
+//             return ResponseEntity.status(401).build();
+//         }
+        _MonthlyTransactionMainRecord respDTO = transactionService.getMonthlyTransactionMain(1, year, month); // TODO: sessionUser.getId()
 
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     // 가계부 저장
-    @PostMapping("/api/transactions")
+    @PostMapping("/transactions") // TODO: /api
     public ResponseEntity<?> saveTransaction(@RequestBody _SaveTransactionRecord reqRecord) {
-         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
-         if (sessionUser == null) {
-             return ResponseEntity.status(401).build();
-         }
-        _SaveTransactionRespRecord respDTO = transactionService.saveTransaction(reqRecord, sessionUser.getId());
+//         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+//         if (sessionUser == null) {
+//             return ResponseEntity.status(401).build();
+//         }
+        _SaveTransactionRespRecord respDTO = transactionService.saveTransaction(reqRecord, 1); // TODO: sessionUser.getId()
 
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     // 가계부 수정
-    @PutMapping("/api/transactions/{transactionId}")
+    @PutMapping("/transactions/{transactionId}") // TODO: /api
     public ResponseEntity<?> updateTransaction(@PathVariable("transactionId") Integer transactionId, @RequestBody _UpdateTransactionRecord reqRecord) {
-         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
-         if (sessionUser == null) {
-             return ResponseEntity.status(401).build();
-         }
+//         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+//         if (sessionUser == null) {
+//             return ResponseEntity.status(401).build();
+//         }
         _UpdateTransactionRespRecord response = transactionService.updateTransaction(transactionId, reqRecord);
 
         return ResponseEntity.ok(new ApiUtil<>(response));
     }
 
     // 가계부 삭제
-    @DeleteMapping("/api/transactions/{transactionId}")
+    @DeleteMapping("/transactions/{transactionId}") // TODO: /api
     public ResponseEntity<?> deleteTransaction(@PathVariable("transactionId") Integer transactionId) {
-         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
-         if (sessionUser == null) {
-             return ResponseEntity.status(401).build();
-         }
-        transactionService.deleteTransaction(transactionId, sessionUser.getId());
+//         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+//         if (sessionUser == null) {
+//             return ResponseEntity.status(401).build();
+//         }
+        transactionService.deleteTransaction(transactionId, 1); // TODO: sessionUser.getId()
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
 }
