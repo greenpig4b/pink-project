ed8ca7c413c71d9930f267758a58fc4cb25f3f91
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-07-16 15:26:45 +0900

    이넘에 이모지 추가

--- src/main/java/com/pinkproject/transaction/enums/CategoryIn.java
+++ src/main/java/com/pinkproject/transaction/enums/CategoryIn.java
@@ -1,20 +1,26 @@
 package com.pinkproject.transaction.enums;
 
 public enum CategoryIn {
-    SALARY("월급"),
-    SIDE_INCOME("부수입"),
-    ALLOWANCE("용돈"),
-    BONUS("상여"),
-    FINANCIAL_INCOME("금융소득"),
-    OTHER("기타");
+    SALARY("월급", "\uD83D\uDCB0"), // 💰
+    SIDE_INCOME("부수입", "\uD83D\uDCB8"), // 💸
+    ALLOWANCE("용돈", "\uD83E\uDD11"), // 🤑
+    BONUS("상여", "\uD83C\uDFC5"), // 🏅
+    FINANCIAL_INCOME("금융소득", "\uD83C\uDFE6"), // 🏦
+    OTHER("기타", "\uD83C\uDFB8"); // 🎸
 
     private final String korean;
+    private final String emoji;
 
-    CategoryIn(String korean) {
+    CategoryIn(String korean, String emoji) {
         this.korean = korean;
+        this.emoji = emoji;
     }
 
     public String getKorean() {
         return korean;
+    }
+
+    public String getEmoji() {
+        return emoji;
     }
 }
