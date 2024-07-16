77491c201d52dce068b1624376c1631aaf4eb25b
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-07-16 17:55:00 +0900

    테스트 진행중

--- src/test/java/com/pinkproject/faq/FaqRepositoryTest.java
+++ src/test/java/com/pinkproject/faq/FaqRepositoryTest.java
@@ -23,67 +23,47 @@ public class FaqRepositoryTest {
 
 
     @Test
-    public void findAllWithAdmin_test(){
-
-        //given
-        Admin admin = new Admin();
-        admin.setUsername("admin");
-        admin.setPassword("password");
-        adminRepository.save(admin);
-
-
-        //when
-        Faq faq1 = new Faq();
-        faq1.setTitle("자주 찾는 질문 1");
-        faq1.setContent("자주 찾는 질문 1의 내용");
-        faq1.setAdmin(admin);
-        faqRepository.save(faq1);
-
-        Faq faq2 = new Faq();
-        faq2.setTitle("자주 찾는 질문 2");
-        faq2.setContent("자주 찾는 질문 2의 내용");
-        faq2.setAdmin(admin);
-        faqRepository.save(faq2);
-
-        Faq faq3 = new Faq();
-        faq3.setTitle("자주 찾는 질문 3");
-        faq3.setContent("자주 찾는 질문 3의 내용");
-        faq3.setAdmin(admin);
-        faqRepository.save(faq3);
-
-        Faq faq4 = new Faq();
-        faq4.setTitle("자주 찾는 질문 4");
-        faq4.setContent("자주 찾는 질문 4의 내용");
-        faq4.setAdmin(admin);
-        faqRepository.save(faq4);
-
-        Faq faq5 = new Faq();
-        faq5.setTitle("자주 찾는 질문 5");
-        faq5.setContent("자주 찾는 질문 5의 내용");
-        faq5.setAdmin(admin);
-        faqRepository.save(faq5);
+    public void findAllWithAdmin_test() {
 
 
 
+        // 페이징 설정
         Pageable pageable = PageRequest.of(0, 5);
         Page<Faq> faqPage = faqRepository.findAllWithAdmin(pageable);
 
-        //then
+        // then
         assertNotNull(faqPage);
-        assertEquals(5, faqPage.getTotalElements());
-        assertEquals(5, faqPage.getNumberOfElements());
-        assertEquals("자주 찾는 질문 1", faqPage.getContent().get(0).getTitle());
-        assertEquals("자주 찾는 질문 2", faqPage.getContent().get(1).getTitle());
-        assertEquals("자주 찾는 질문 3", faqPage.getContent().get(2).getTitle());
-        assertEquals("자주 찾는 질문 4", faqPage.getContent().get(3).getTitle());
-        assertEquals("자주 찾는 질문 5", faqPage.getContent().get(4).getTitle());
-        assertEquals(admin, faqPage.getContent().get(0).getAdmin().getUsername());
-        assertEquals(admin, faqPage.getContent().get(1).getAdmin().getUsername());
-        assertEquals(admin, faqPage.getContent().get(2).getAdmin().getUsername());
-        assertEquals(admin, faqPage.getContent().get(3).getAdmin().getUsername());
-        assertEquals(admin, faqPage.getContent().get(4).getAdmin().getUsername());
-
+        assertEquals(5, faqPage.getNumberOfElements()); // 페이지당 5개의 항목
+        assertEquals("서비스 이용약관", faqPage.getContent().get(0).getTitle());
+        assertEquals("다중 계정 사용", faqPage.getContent().get(1).getTitle());
+        assertEquals("계정 잠금 해제", faqPage.getContent().get(2).getTitle());
+        assertEquals("회원 탈퇴 방법", faqPage.getContent().get(3).getTitle());
+        assertEquals("프로모션 코드 사용 방법", faqPage.getContent().get(4).getTitle());
+//        assertEquals("비밀번호 변경", faqPage.getContent().get(4).getTitle());
+        assertEquals("admin", faqPage.getContent().get(0).getAdmin().getUsername());
+        assertEquals("admin", faqPage.getContent().get(1).getAdmin().getUsername());
+        assertEquals("admin", faqPage.getContent().get(2).getAdmin().getUsername());
+        assertEquals("admin", faqPage.getContent().get(3).getAdmin().getUsername());
+        assertEquals("admin", faqPage.getContent().get(4).getAdmin().getUsername());
     }
 
 
+    @Test
+    public void findByKeywordWithFaq_test() {
+
+        admin = adminRepository.findByUsername("admin");
+        //given
+        String keyword = "방법";
+        Pageable pageable = PageRequest.of(0, 5);
+
+        //when
+        Page<Faq> faqPage = faqRepository.findByKeywordWithFaq(keyword, pageable);
+
+        //then
+        assertNotNull(faqPage);
+        assertEquals(7, faqPage.getTotalElements()); // "방법"이 포함된 FAQ는 3개
+        assertEquals("회원 탈퇴 방법", faqPage.getContent().get(0).getTitle());  // 최근 생성된 항목
+        assertEquals("데이터 백업 방법", faqPage.getContent().get(1).getTitle());
+        assertEquals("가계부 작성 방법", faqPage.getContent().get(2).getTitle());
+    }
 }
